b44dea82a5f23a386a1f4376984251265da162c5
Grebla Andrei <154756433+AndGG1@users.noreply.github.com>
2024-10-28 00:11:18 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/DTOS/SignInView.java

--- src/Database/Initialization/Database_Initialization.java
+++ src/Database/Initialization/Database_Initialization.java
@@ -3,22 +3,30 @@ package Database.Initialization;
 import com.mysql.cj.jdbc.DatabaseMetaData;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class Database_Initialization {
 
     private static String USE_SCHEMA = "USE storefront";
     private static int MYSQL_DB_NOT_FOUND = 1049;
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Properties props = new Properties();
+        props.load(Files.newInputStream(Path.of("storefront.properties"),
+                StandardOpenOption.READ));
+        
         var ds = new MysqlDataSource();
         ds.setServerName("localhost");
         ds.setPort(3306);
-        ds.setUser(System.getenv("MYSQLUSER"));
-        ds.setPassword(System.getenv("MYSQLPASS"));
+        ds.setUser(props.getProperty("user"));
+        ds.setPassword(props.getProperty("pass"));
 
         try (Connection conn = ds.getConnection()) {
 
@@ -27,9 +35,10 @@ public class Database_Initialization {
             System.out.println(metaData.getConnection());
 
             if (!checkSchema(conn)) {
-                System.out.println("storefront schema does not exist!");
+                System.out.println("storefront schema does not exist! :)");
                 setUpSchema(conn);
-            }
+            } else System.out.println("The Schema does already exist! :(");
+            
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -55,29 +64,26 @@ public class Database_Initialization {
     }
 
     private static void setUpSchema(Connection conn) throws SQLException {
-
         String createSchema = "CREATE SCHEMA storefront";
 
         String createUser = """
-                CREATE TABLE storefront.user (
-                user_id int NOT NULL AUTOINCREMENT,
-                user_name text NOT NULL,
-                user_pass text NOT NULL,
-                user_link text,
-                user_img text,
-                user_description,
-                PRIMARY KEY (user_id)
-                )
-                """;
-
+            CREATE TABLE storefront.user (
+            user_id INT NOT NULL AUTO_INCREMENT,
+            user_name TEXT NOT NULL,
+            user_pass TEXT NOT NULL,
+            user_link TEXT,
+            user_img TEXT,
+            user_description TEXT,
+            PRIMARY KEY (user_id)
+            )
+            """;
         try (Statement st = conn.createStatement()) {
-
             System.out.println("Creating storefront db.");
             st.execute(createSchema);
             if (checkSchema(conn)) {
                 st.execute(createUser);
                 System.out.println("Successfully created The User!");
-            }
+            } else System.out.println("User already exists as a Table!");
         } catch (SQLException e) {
             e.printStackTrace();
         }
